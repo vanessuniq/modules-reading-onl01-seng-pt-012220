26b4847a4e1855bd67be7680229a6be4ab5ae980
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-27 08:02:22 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,7 +3,8 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
 class Dancer
-  
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
  
   attr_accessor :name
  
